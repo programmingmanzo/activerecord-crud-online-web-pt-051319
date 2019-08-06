abc4f5fff7243441610b7f3a3a706ff7fcb9fdbf
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-06 21:28:46 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_203214) do
+ActiveRecord::Schema.define(version: 2019_08_06_212740) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
 
 end
